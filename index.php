b1012ab8e5e91cbde7b59751819490ece8ff2136
jprodrigues70 <joaorodrigues@infojr.com.br>
2016-01-17 14:39:16 -0300

    Remedia problema de responsividade com telas maiores que 1366 x 768

--- index.php
+++ index.php
@@ -10,21 +10,23 @@
 	</head>
 	<body>
 		<section class="aside">
-			<div class="text">
-				<h2>Versionamento com</h2>
-				<h1 class="github">Git & GitHub</h1>
-                <h2 class="right">Dia 19 de Fevereiro às 14hs</h2>
-                <a href="#subscribe"><button id="sign-up" class="aside-btn"><h2><i class="octicon octicon-git-pull-request"></i> Inscreva-se</h2></button></a>
-	    	</div>
-	    	<div class="professorctocat">
-	    	    <img src="assets/img/professorctocat-git.svg" alt="octocat">
-	    	</div>
-	    	<div class="triangle"></div>
-	    </section>
-		<section class="about">
+            <main>
+                <div class="text">
+                    <h2>Versionamento com</h2>
+                    <h1 class="github">Git & GitHub</h1>
+                    <h2 class="right">Dia 19 de Fevereiro às 14hs</h2>
+                    <a href="#subscribe"><button id="sign-up" class="aside-btn"><h2><i class="octicon octicon-git-pull-request"></i> Inscreva-se</h2></button></a>
+                </div>
+                <div class="professorctocat">
+                    <img src="assets/img/professorctocat-git.svg" alt="octocat">
+                </div>
+                <div class="triangle"></div>
+           </main>
+        </section>
+        <section class="about">
 	    	<div class="triangle-invert"></div>
-			<h1>Git & GitHub</h1>
             <article>
+			    <h1>Git & GitHub</h1>
                 <p><b>Git</b> é um sistema de controle de versão gratuíto e open source projetado para lidar com tudo. Dos pequenos aos grandes projetos, com velocidade e eficiência.
                 </p>
                 <br>
@@ -59,65 +61,69 @@
             </main>
         </section>
         <section id="subscribe" class="inscription">
-            <h1>Inscreva-se!</h1>
-            <?php if (isset($_SESSION['msg'])): ?>
-                <div class="msg <?php echo $_SESSION['type']; ?>"><?php echo $_SESSION['msg']; ?></div>
-                <?php unset($_SESSION['msg']); unset($_SESSION['type']); ?>
-            <?php endif; ?>
-            <div class="msg-fixed">
-                <h3>Taxa de inscrição: 1Kg de alimento não perecível, que deve ser entregue no dia da capacitação.</h3>
-            </div>
-            <form action="controllers/user" method="POST" enctype="multipart/form-data">
-                <input id="name" class="field left" title="Insira o seu nome aqui." type="text" name="name" placeholder="Qual o seu nome?" required="required">
-                <input id="email" class="field right" title="Insira o seu e-mail aqui." type="email" name="email" placeholder="Qual o seu e-mail?" value="" required="required">
-                <input id="phone" class="field left" title="Insira o seu telefone aqui." name="phone" placeholder="E o seu telefone?" value="" required="required">
-                <input class="field right" title="Insira o seu Número de Matrícula aqui." type="number" min="9" name="registry" placeholder="Número de matrícula" required="required">
-                <select class="field left" name="course" required="required">
-                    <option value="">Selecione seu Curso</option>
-                    <option value="CC">Ciência da Computação</option>
-                    <option value="SI">Sistemas de Informação</option>
-                    <option value="LC">Licenciatura em Computação</option>
-                    <option value="EC">Engenharia de Computação</option>
-                    <option value="BI">Bacharelado Interdisciplinar C&T</option>
-                    <option value="OT">Outro</option>
-                </select>
-                <select class="field right" name="semester" required="required">
-                    <option value="">Selecione seu Semestre</option>
-                    <option value="1">1°Semestre</option>
-                    <option value="2">2°Semestre</option>
-                    <option value="3">3°Semestre</option>
-                    <option value="4">4°Semestre</option>
-                    <option value="A4">Acima do 4°Semestre</option>
-                </select>
-                <p>* Após a inscrição, você receberá um e-mail de confirmação. Verifique também a sua caixa de spam. Se NÃO receber o e-mail, contate-nos através do contato@infojr.com.br.</p>
-                <div class="g-recaptcha" data-sitekey=""></div>
-                <button class="send-btn" name="action" value="create" type="submit"><i class="octicon octicon-git-merge"></i> Enviar</button>
-            </form>
+            <main>
+                <h1>Inscreva-se!</h1>
+                <?php if (isset($_SESSION['msg'])): ?>
+                    <div class="msg <?php echo $_SESSION['type']; ?>"><?php echo $_SESSION['msg']; ?></div>
+                    <?php unset($_SESSION['msg']); unset($_SESSION['type']); ?>
+                <?php endif; ?>
+                <div class="msg-fixed">
+                    <h3>Taxa de inscrição: 1Kg de alimento não perecível, que deve ser entregue no dia da capacitação.</h3>
+                </div>
+                <form action="controllers/user" method="POST" enctype="multipart/form-data">
+                    <input id="name" class="field left" title="Insira o seu nome aqui." type="text" name="name" placeholder="Qual o seu nome?" required="required">
+                    <input id="email" class="field right" title="Insira o seu e-mail aqui." type="email" name="email" placeholder="Qual o seu e-mail?" value="" required="required">
+                    <input id="phone" class="field left" title="Insira o seu telefone aqui." name="phone" placeholder="E o seu telefone?" value="" required="required">
+                    <input class="field right" title="Insira o seu Número de Matrícula aqui." type="number" min="9" name="registry" placeholder="Número de matrícula" required="required">
+                    <select class="field left" name="course" required="required">
+                        <option value="">Selecione seu Curso</option>
+                        <option value="CC">Ciência da Computação</option>
+                        <option value="SI">Sistemas de Informação</option>
+                        <option value="LC">Licenciatura em Computação</option>
+                        <option value="EC">Engenharia de Computação</option>
+                        <option value="BI">Bacharelado Interdisciplinar C&T</option>
+                        <option value="OT">Outro</option>
+                    </select>
+                    <select class="field right" name="semester" required="required">
+                        <option value="">Selecione seu Semestre</option>
+                        <option value="1">1°Semestre</option>
+                        <option value="2">2°Semestre</option>
+                        <option value="3">3°Semestre</option>
+                        <option value="4">4°Semestre</option>
+                        <option value="A4">Acima do 4°Semestre</option>
+                    </select>
+                    <p>* Após a inscrição, você receberá um e-mail de confirmação. Verifique também a sua caixa de spam. Se NÃO receber o e-mail, contate-nos através do contato@infojr.com.br.</p>
+                    <div class="g-recaptcha" data-sitekey=""></div>
+                    <button class="send-btn" name="action" value="create" type="submit"><i class="octicon octicon-git-merge"></i> Enviar</button>
+                </form>
+            </main>
         </section>
         <footer class="a">
-            <div class="about-us">
-                <a href="http://www.infojr.com.br" target="_blank"><img src="assets/img/infojr-lb-flat.svg" alt=""></a>
-                <p>Empresa Júnior de Informática da Universidade Federal da Bahia, fundada em 26 de Janeiro de 1998, formada por alunos dos cursos de computação.</p>
-            </div>
-            <div class="contact-us">
-                <div class="contact">
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                    </svg><p>contato@infojr.com.br</p>
+            <main>
+                <div class="about-us">
+                    <a href="http://www.infojr.com.br" target="_blank"><img src="assets/img/infojr-lb-flat.svg" alt=""></a>
+                    <p>Empresa Júnior de Informática da Universidade Federal da Bahia, fundada em 26 de Janeiro de 1998, formada por alunos dos cursos de computação.</p>
                 </div>
-                <div class="contact">
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
-                    </svg><p>+55 71 3223-6268</p>
+                <div class="contact-us">
+                    <div class="contact">
+                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                        </svg><p>contato@infojr.com.br</p>
+                    </div>
+                    <div class="contact">
+                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+                        </svg><p>+55 71 3223-6268</p>
+                    </div>
+                    <a href="https://facebook.com/infojrnews" class="contact" target="_blank">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path d="M19,4V7H17A1,1 0 0,0 16,8V10H19V13H16V20H13V13H11V10H13V7.5C13,5.56 14.57,4 16.5,4M20,2H4A2,2 0 0,0 2,4V20A2,2 0 0,0 4,22H20A2,2 0 0,0 22,20V4C22,2.89 21.1,2 20,2Z" />
+                        </svg><p> /infojrnews</p>
+                    </a>
                 </div>
-                <a href="https://facebook.com/infojrnews" class="contact" target="_blank">
-                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path d="M19,4V7H17A1,1 0 0,0 16,8V10H19V13H16V20H13V13H11V10H13V7.5C13,5.56 14.57,4 16.5,4M20,2H4A2,2 0 0,0 2,4V20A2,2 0 0,0 4,22H20A2,2 0 0,0 22,20V4C22,2.89 21.1,2 20,2Z" />
-                    </svg><p> /infojrnews</p>
-                </a>
-            </div>
+            </main>
         </footer>
         <footer class="b">
             <a href="www.infojr.com.br" target="_blank"><h4>© InfoJr UFBA - 2016</h4></a>
